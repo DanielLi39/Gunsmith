6f8b8d7c679181b44c67cc65b7108af112a29deb
Daniel Li <daniel39@illinois.edu>
2024-01-15 16:36:54 -0600

    I might've screwed up

---
+++ auth.js
@@ -0,0 +1,48 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import Credentials from 'next-auth/providers/credentials';
+import { MongoClient } from "mongodb";
+import { ServerApiVersion } from "mongodb";
+import bcrypt from 'bcrypt';
+
+async function getUser(username) {
+    const client = new MongoClient(process.env.MONGODB_URI, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+    try {
+        await client.connect();
+
+        const user = await client.db("mw3_gunbuilds").collection("users").findOne( {username: username} );
+
+        return user;
+    } catch (error) {
+        console.log("Failed to fetch user:", error);
+        throw new Error('Failed to fetch user.');
+    }
+}
+
+
+//No idea what this is doing
+export const { auth, signIn, signOut } = NextAuth({
+    ...authConfig,
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const { username, password } = credentials;
+                const user = await getUser(username);
+                if (!user) return null;
+
+                console.log(user);
+                
+                const passwordsMatch = await bcrypt.compare(password, user.password);
+                if (passwordsMatch) return user;
+                return null;
+            }
+        })
+    ]
+});
