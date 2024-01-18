b44448fb9cbc84fa69dc73888f00340e35e7b38a
Daniel Li <daniel39@illinois.edu>
2024-01-17 22:13:03 -0600

    Imported and wrapped app with ClerkProvider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/clerk-js';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -10,10 +11,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-        </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {children}
+          </body>
+      </html>
+    </ClerkProvider>
   )
 }
