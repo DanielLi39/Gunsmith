e670130ae8f021348810da2444a45e607adba9ce
Daniel Li <daniel39@illinois.edu>
2024-01-21 15:13:19 -0600

    Created scrollable table, though styling is slightly off

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 TO-DO:
 - Add form state to the Gun Display
 - Add error messages when selecting guns, attachments, and writing to database
+- Make an actual good UI by changing colors and shit
+- Refactor server actions 
+- Add the rest of the database data (will take forever)
+- Add randomizer
+- Add table search functionality
+- Document the rest of the components
--- src/app/builder/components/BuildList.js
+++ src/app/builder/components/BuildList.js
@@ -18,6 +18,8 @@ export default function BuildList( {sendToGunsmith} ) {
         if (result.success) {
             setBuilds(result.data);
             setIsOpen(true);
+        } else {
+            //Some error thing
         }
     }
 
@@ -26,41 +28,58 @@ export default function BuildList( {sendToGunsmith} ) {
         console.log(result);
         if (result.success) {
             await receiveCursor();
+        } else {
+            //Some error thing
         }
     }
 
     return (
-        <div className="h-screen bg-neutral-900 text-white">
-            <input type="text"/>
-            <button type="button" onClick={receiveCursor}>Load builds</button>
-            <table className={`${isOpen ? '' : 'hidden'}`}>
-                <thead>
-                    <tr className="py-2 border-2 border-white">
-                        <td>Created By</td>
-                        <td>Name</td>
-                        <td>Gun</td>
-                        <td>Camo</td>
-                        <td>Attachments</td>
-                    </tr>
-                </thead>
-                <tbody className="">
-                    {builds.map(build => {
-                        return (
-                            <tr key={build._id}
-                                className="py-2 border-2 border-white">
-                                <td>{build.author}</td>
-                                <td>{build.name}</td>
-                                <td>{build.gunName}</td>
-                                <td>{build.camo}</td>
-                                <td colSpan="5">{build.attachments.toString()}</td>
-                                <td className="cursor-pointer" onClick={() => sendToGunsmith(build.gunName, build.attachments)}>Load</td>
-                                <td className="cursor-pointer" onClick={() => deleteItem(build._id)}>Delete</td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
-            
+        <div className="h-screen bg-gradient-to-b from-neutral-900 via-neutral-800 via-60% to-red-800 text-white">
+            <div className="pt-5 overflow-clip">
+                <input type="text"/>
+                <button type="button" onClick={receiveCursor}>Load builds</button>
+                
+                <table className={`${isOpen ? '' : 'hidden'}`}>
+                    <thead>
+                        <tr className="py-2 border-2 border-white bg-neutral-700">
+                            <td className="w-[100px]">Created By</td>
+                            <td className="w-[100px] bg-neutral-500"> Name</td>
+                            <td className="w-[100px]"> Gun</td>
+                            <td className="w-[100px] bg-neutral-500"> Camo</td>
+                            <td className="w-[500px]"> Attachments</td>
+                            <td className="w-[75px] bg-neutral-500"></td>
+                            <td className="w-[100px] bg-neutral-400"></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="border-2 border-white">
+                            <td colSpan="7">
+                                <div className="h-[500px] block overflow-y-auto overflow-x-clip bg-neutral-700">
+                                    <table>
+                                        <tbody>
+                                            {builds.map(build => {
+                                                return (
+                                                    <tr key={build._id}
+                                                        >
+                                                        <td className="w-[100px]">{build.author}</td>
+                                                        <td className="w-[100px] bg-neutral-500">{build.name}</td>
+                                                        <td className="w-[100px]">{build.gunName}</td>
+                                                        <td className="w-[100px] bg-neutral-500">{build.camo}</td>
+                                                        <td className="w-[500px]">{build.attachments.toString()}</td>
+                                                        <td className="cursor-pointer w-[75px] bg-neutral-500" onClick={() => sendToGunsmith(build.gunName, build.attachments)}>Load</td>
+                                                        <td className="cursor-pointer w-[75px] bg-neutral-400" onClick={() => deleteItem(build._id)}>Delete</td>
+                                                    </tr>
+                                                );
+                                            })}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </td>
+                        </tr>
+                        
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
